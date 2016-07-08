a0a18a5b5f39efd26e476bc41ea9a60657bcbb29
Sergey Melentyev <sergey.melentyev@gmail.com>
2016-07-08 18:54:57 +0400

    Create functions.h

---
+++ functions.h
@@ -0,0 +1,16 @@
+//
+// Created by Melentyev on 07.07.16.
+//
+
+#include <iostream>
+
+#ifndef CPP_LANG_FUNCTION_H
+#define CPP_LANG_FUNCTION_H
+
+void func(int x, int& y, const int& z) {
+    // int x passed by value
+    // int& y passed by reference
+    // const int& z passed by immutable reference
+}
+
+#endif //CPP_LANG_FUNCTION_H
