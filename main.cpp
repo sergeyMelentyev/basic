b7567e79351cd37f2281efac147e17693a5758f9
Sergey Melentyev <sergey.melentyev@gmail.com>
2016-07-21 16:23:47 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,17 +10,20 @@
 using namespace std;
 int summ_array(int values[], int size);                            // function prototype
 
-/* pointer vs reference */
+
+/*** POINTER VS REFERENCE ***/
     int any_function_name(int x) { return x+1; }
     void any_function_name(int* x) { ++*x; }
     void any_function_name(int& x) { ++x; }
+
 
 int main() {
     constexpr int max_value = 100;									// constant expression must be assigned
     const int min_value = 100 / max_value;							// constant classic can be assign at compile time
     double d {2.7};													// universal and uniform initialization
 
-/* ENUM TYPE */
+
+/*** ENUM TYPE ***/
     enum RainbowColor {
         RC_RED = 1,
         RC_ORANGE = 3,
@@ -28,7 +31,8 @@ int main() {
     };
     RainbowColor choose_color = RC_ORANGE;
 
-/* STRUCT TYPE */
+
+/*** STRUCT TYPE ***/
     struct PlayerInfo {
         int skill_level;
         string name;
@@ -43,10 +47,19 @@ int main() {
         // use loops in order to iterate inside an array of structs
     }
 
-    EnemySpaceShip enemy = getNewEnemy();                           // check struct.h file
+    EnemySpaceShip enemy = getNewEnemy();                           // check struct.h file for more details
     enemy = upgradeWeapon(enemy);
 
-/* ARRAY TYPE */
+    EnemyStarShip* p_enemy_first = get_new_enemy();                 // check struct.h file for more details
+    EnemyStarShip* p_enemy_second = get_new_enemy();
+    EnemyStarShip* p_current = p_enemies;                           // pointer to the current element of array
+    while (p_current != NULL) {
+        upgrade_weapon(p_current);
+        p_current = p_current -> p_next_enemy;                      // assign pointer to the next element of array
+    }
+
+
+/*** ARRAY TYPE ***/
     const int max_number = 10;                                      // length of array must be set at compile time
     int simple_array[max_number];                                   // classic array init
     int two_dimension_array[3][3];                                  // two dimension array
@@ -58,48 +71,68 @@ int main() {
     pointer_array[0] = 1;                                           // change value of first element
     pointer_array += 1;                                             // pointer to the address of second element
 
-/* POINTERS IN DEPTH */
+
+/*** POINTERS IN DEPTH ***/
+    int* null_pointer = nullptr;                                    // init pointer with null for safety
+    int* empty_pointer;                                             // declare pointer without init (bad)
     int any_var_name = 17;
     int* any_pointer_name = &any_var_name;                          // pointer of type int
-    *any_pointer_name = 27; /* <= the same as => */ any_pointer_name[0] = 27;
+    *any_pointer_name = 27;
+    /* <= the same as => */
+    any_pointer_name[0] = 27;
 
-    int* new_name_0 = nullptr;                                      // init pointer with null for safety
-    int* new_name_1;                                                // declare pointer without init (bad)
-    int* new_name_2 = new int;                                      // alloc of dynamic ram without init
-    int* new_name_3 = new int(5);                                   // init pointer with value of 5
-    int* new_name_4 = new int{5};
-    int* new_name_5 = new int[5];                                   // alloc of dynamic ram for array of 5 int
-    int* new_name_6 = new int[5] {0, 1, 2, 3, 4};                   // alloc and init array of 5 int
 
-    int* copy_shallow = new_name_4;                                 // shallow copy, the same object
-    int* deep_copy = new int{*new_name_4};                          // deep copy, different object
-
-    delete new_name_5;                                              // dealloc memory for one obj
-    delete[] new_name_6;                                            // dealloc memory for array
-
-    int n = 2;
-    double* p = new double[n];                                      // allocation in dynamic ram for n values
-    cout << "Dynamic alloc = " << *p << "\n";                       // *p or p[0] print first element
-    int* i = new int;                                               // alloc in dynamic ram for array of one int
-    cout << "Dynamic alloc = " << *i << "\n";
-
-/* POINTER VS REFERENCE */
+/*** POINTER VS REFERENCE ***/
     int number_one = 10; int& ref = number_one; ref = 15;           // auto dereferenced pointer, called reference
     int number_two = 10; int* poi = &number_two; *poi = 15;         // classic pointer
 
-/* VECTOR IN DEPTH */
+
+/*** MEMORY MANAGEMENT ***/
+    int* new_name_0 = new int;                                      // alloc of dynamic memory
+    delete new_name_0;                                              // dealloc memory and set pointer equal to NULL
+    new_name_0 = NULL;
+
+    int* new_name_1 = new int(5);                                   // alloc dynamic memory and init with int
+    int* new_name_2 = new int{5};
+
+    int* new_name_3 = new int[5];                                   // alloc dynamic memory and init with array of ints
+    int* new_name_4 = new int[5] {0, 1, 2, 3, 4};
+    delete[] new_name_4;                                            // dealloc memory for array
+
+    int* copy_shallow = new_name_2;                                 // shallow copy, the same object
+    int* deep_copy = new int{*new_name_2};                          // deep copy, different object
+
+    int** p_p_new_name;
+    p_p_new_name = new int*[3];                                     // alloc dynamic memory for multidimensional array
+    for (int i = 0; i < 3; i++) {
+        p_p_new_name[i] = new int[3];
+    }
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            p_p_new_name[i][j] = 0;
+        }
+    }
+    for (int i = 0; i < 3; i++) {                                   // dealloc memory in reverse way
+        delete[] p_p_new_name[i];
+    }
+    delete[] p_p_new_name;
+
+
+/*** VECTOR IN DEPTH ***/
     vector<int> v {10, 3, 2, 7};                                    // vector init with passed elements
     v.push_back(4);                                                 // add value to the end of the vector
     vector<double> vd;                                              // vector declaration
     vector<string> s(4);                                            // vector declaration with four default elements
 
-/* custom data types in depth */
+
+/*** CLASSES ***/
     custom_data_types token {'C', 14.0};                            // class init
     token.show_private_data();
     token.show_more();
 
     return 0;
 }
+
 
 int summ_array(int values[], int size) {                            // pass array as an argument to the func
     int sum = 0;
