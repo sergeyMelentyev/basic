90a240585ca63d3d42b65f0118860d9e4334ce3c
Sergey Melentyev <sergey.melentyev@gmail.com>
2016-07-21 19:34:42 +0400

    Update struct.h

--- struct.h
+++ struct.h
@@ -50,4 +50,48 @@
         p_ship -> weapon_power += 10;
     }
 
+
+/*** BINARY TREES ***/
+    struct node {
+        int key_value;
+        node* p_left;
+        node* p_right;
+    };
+
+    node* insert(node* p_tree, int key) {
+        if (p_tree == NULL) {                                                   // find empty tree, add new node
+            node* p_new_tree = new node;
+            p_new_tree -> p_left = NULL;
+            p_new_tree -> p_right = NULL;
+            p_new_tree -> key_value = key;
+            return p_new_tree;
+        }
+        if (key < p_tree -> key_value) {
+            p_tree -> p_left = insert(p_tree -> p_left, key);
+        } else {
+            p_tree -> p_right = insert(p_tree -> p_right, key);
+        }
+        return p_tree;
+    }
+
+    node* search(node* p_tree, int key) {
+        if (p_tree == NULL) {
+            return NULL;
+        } else if (key == p_tree -> key_value) {
+            return p_tree;
+        } else if (key < p_tree -> key_value) {
+            return search(p_tree -> p_left, key);
+        } else {
+            return search(p_tree -> p_right, key);
+        }
+    }
+
+    void destroy_tree(node* p_tree){
+        if (p_tree != NULL) {
+            destroy_tree(p_tree -> p_left);
+            destroy_tree(p_tree -> p_right);
+            delete p_tree;
+        }
+    }
+
 #endif //CPP_LANG_STRUCT_H
